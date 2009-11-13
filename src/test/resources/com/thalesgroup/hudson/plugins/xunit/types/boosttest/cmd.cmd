5135a0ee8d8cb83b215413589a07d59a99513d82
gbois <gbois@dev.java.net>
2009-11-13 16:00:05 +0000

    Remove the aunit support (The Aunit support will be provided by AUnit) + Updated the cmd command for boostest library

--- src/test/resources/com/thalesgroup/hudson/plugins/xunit/types/boosttest/cmd.cmd
+++ src/test/resources/com/thalesgroup/hudson/plugins/xunit/types/boosttest/cmd.cmd
@@ -1,6 +1,9 @@
 ./a.exe --report_format=XML --report_level=detailed
+-->produces testresult.xml
 
 ./a.exe --output_format=XML --log_level=all
+-->produces testlog-testresult.xml
 
 Correct for plugin with only testlog
 ./a.exe --output_format=XML --log_level=all --report_level=no
+-->produces testlog.xml
