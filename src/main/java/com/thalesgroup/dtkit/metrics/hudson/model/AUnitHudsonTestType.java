ffff2fb9d3591fcbfd532fabd57cb01eacdd898e
gbois <gbois@dev.java.net>
2010-09-23 14:12:27 +0000

    Added backward compatibilty with previous metrics

---
+++ src/main/java/com/thalesgroup/dtkit/metrics/hudson/model/AUnitHudsonTestType.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Thales Corporate Services SAS                             *
+ * Author : Gregory Boissinot                                                   *
+ *                                                                              *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy *
+ * of this software and associated documentation files (the "Software"), to deal*
+ * in the Software without restriction, including without limitation the rights *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
+ * copies of the Software, and to permit persons to whom the Software is        *
+ * furnished to do so, subject to the following conditions:                     *
+ *                                                                              *
+ * The above copyright notice and this permission notice shall be included in   *
+ * all copies or substantial portions of the Software.                          *
+ *                                                                              *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
+ * THE SOFTWARE.                                                                *
+ *******************************************************************************/
+
+package com.thalesgroup.dtkit.metrics.hudson.model;
+
+import com.thalesgroup.dtkit.metrics.hudson.api.descriptor.TestTypeDescriptor;
+import com.thalesgroup.dtkit.metrics.hudson.api.type.TestType;
+
+
+public class AUnitHudsonTestType extends TestType {
+
+    public AUnitHudsonTestType(String pattern, boolean faildedIfNotNew, boolean deleteOutputFiles) {
+        super(pattern, faildedIfNotNew, deleteOutputFiles);
+    }
+
+    public TestTypeDescriptor<? extends TestType> getDescriptor() {
+        return null;
+    }
+
+//    public static class DescriptorImpl extends TestTypeDescriptor<AUnitHudsonTestType> {
+//
+//        public DescriptorImpl() {
+//            super(AUnitHudsonTestType.class, com.thalesgroup.dtkit.junit.AUnit.class);
+//        }
+//
+//        public String getId() {
+//            return "class com.thalesgroup.dtkit.junit.AUnit";
+//        }
+//    }
+
+    public Object readResolve() {
+        return new AUnitJunitHudsonTestType(this.getPattern(), this.isFaildedIfNotNew(), this.isDeleteOutputFiles());
+    }
+}
---
+++ src/main/java/com/thalesgroup/dtkit/metrics/hudson/model/TUSARHudsonTestType.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Thales Corporate Services SAS                             *
+ * Author : Gregory Boissinot                                                   *
+ *                                                                              *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy *
+ * of this software and associated documentation files (the "Software"), to deal*
+ * in the Software without restriction, including without limitation the rights *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
+ * copies of the Software, and to permit persons to whom the Software is        *
+ * furnished to do so, subject to the following conditions:                     *
+ *                                                                              *
+ * The above copyright notice and this permission notice shall be included in   *
+ * all copies or substantial portions of the Software.                          *
+ *                                                                              *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
+ * THE SOFTWARE.                                                                *
+ *******************************************************************************/
+
+package com.thalesgroup.dtkit.metrics.hudson.model;
+
+import com.thalesgroup.dtkit.junit.Tusar;
+import com.thalesgroup.dtkit.metrics.hudson.api.descriptor.TestTypeDescriptor;
+import com.thalesgroup.dtkit.metrics.hudson.api.type.TestType;
+
+
+public class TUSARHudsonTestType extends TestType {
+
+    public TUSARHudsonTestType(String pattern, boolean faildedIfNotNew, boolean deleteOutputFiles) {
+        super(pattern, faildedIfNotNew, deleteOutputFiles);
+    }
+
+    public TestTypeDescriptor<? extends TestType> getDescriptor() {
+        return null;
+    }
+
+//    public static class DescriptorImpl extends TestTypeDescriptor<TUSARHudsonTestType> {
+//
+//        public DescriptorImpl() {
+//            super(TUSARHudsonTestType.class, Tusar.class);
+//        }
+//
+//        public String getId() {
+//            return "class com.thalesgroup.dtkit.Tusar.";
+//        }
+//    }
+
+    public Object readResolve() {
+        return new CppUnitJunitHudsonTestType(this.getPattern(), this.isFaildedIfNotNew(), this.isDeleteOutputFiles());
+    }
+}
