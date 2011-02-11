fb4d7beae4377eaeea973bec039639bc5a9e7dbb
gbois <gbois@dev.java.net>
2011-02-11 23:22:10 +0000

    Migrated to Jenkins 1.396

--- src/main/java/com/thalesgroup/hudson/plugins/xunit/types/CustomType.java
+++ src/main/java/com/thalesgroup/hudson/plugins/xunit/types/CustomType.java
@@ -63,7 +63,7 @@ public class CustomType extends TestType {
 
         @Override
         public String getId() {
-            throw new UnsupportedOperationException("The descriptor registry with the called getId() method is not used. The descriptor redefines its own getInputMetric() method.");
+            return this.getClass().getName();
         }
 
         @Override
