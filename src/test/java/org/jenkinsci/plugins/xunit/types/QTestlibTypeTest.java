b31a0cd7003cb2af67efdf3950deca2d323bc914
Frederik Gladhorn <frederik.gladhorn@digia.com>
2013-10-25 11:09:50 +0200

    Add time to qtestlib tests
    This is a recent addition for the xml output generated by Qt, so don't
    rely on it being there.

--- src/test/java/org/jenkinsci/plugins/xunit/types/QTestlibTypeTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/QTestlibTypeTest.java
@@ -10,5 +10,6 @@ public class QTestlibTypeTest extends AbstractTest {
     @Test
     public void testTestCase1() throws Exception {
         convertAndValidate(QTestLibInputMetric.class, "qtestlib/testcase1/input.xml", "qtestlib/testcase1/result.xml");
+        convertAndValidate(QTestLibInputMetric.class, "qtestlib/testcase2/input.xml", "qtestlib/testcase2/result.xml");
     }
 }
