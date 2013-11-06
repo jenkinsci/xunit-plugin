cf2f403cd5ba351deacb6092f71cef686546ab82
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-11-06 19:01:34 +0100

    Remove Java 7, not a Jenkins target

--- src/test/java/com/thalesgroup/hudson/plugins/xunit/service/XUnitValidationServiceTest.java
+++ src/test/java/com/thalesgroup/hudson/plugins/xunit/service/XUnitValidationServiceTest.java
@@ -23,112 +23,65 @@
 
 package com.thalesgroup.hudson.plugins.xunit.service;
 
-import java.io.*;
-//import java.nio.file.*;
-//import java.nio.file.FileSystem;
-import org.junit.*;
 import com.thalesgroup.hudson.plugins.xunit.transformer.TempWorkspace;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 /**
  * Unit tests for XUnitValidationService class.
  *
  * @author Maciek Siemczyk
- * 
- * @note Two of the unit tests were disabled as they require Java 7 API.
  */
-public class XUnitValidationServiceTest
-{
+public class XUnitValidationServiceTest {
     /**
-     * Helper class for creating temporary workspace. 
+     * Helper class for creating temporary workspace.
      */
     @Rule
     public TempWorkspace tempWorkspace = new TempWorkspace();
-    
+
     /**
      * System Under Test (SUT).
      */
     private XUnitValidationService xUnitValidationService = new XUnitValidationService();
 
     @Test
-    public void CheckFileIsNotEmpty_GivenEmptyFile_ReturnsFalse() throws Exception
-    {
+    public void CheckFileIsNotEmpty_GivenEmptyFile_ReturnsFalse() throws Exception {
         File testFile = new File(tempWorkspace.getDir(), "empty.txt");
-        testFile.createNewFile();
-        
-        Assert.assertFalse(
-            "CheckFileIsNotEmpty returned true.",
-            xUnitValidationService.checkFileIsNotEmpty(testFile));
-    }
-    
-    @Test
-    public void CheckFileIsNotEmpty_GivenNotEmptyFile_ReturnsTrue() throws Exception
-    {
-        File testFile = CreateNotEmtyFile();
-        
-        Assert.assertTrue(
-            "CheckFileIsNotEmpty returned false.",
-            xUnitValidationService.checkFileIsNotEmpty(testFile));
-    }
-    
-    @Test
-    @Ignore
-    public void CheckFileIsNotEmpty_GivenLinkToEmptyFile_ReturnsFalse() throws Exception
-    {
-        /**
-         * This unit test is disabled as it required Java 7 API.
-         
-        FileSystem filesystem = FileSystems.getDefault();
-        
-        File targetFile = new File(tempWorkspace.getDir(), "empty.txt");;
-        targetFile.createNewFile();
-        
-        Path targetPath = filesystem.getPath(targetFile.getPath());
-        Path linkPath = filesystem.getPath(tempWorkspace.getDir().getPath(), "link.txt");
-        
-        Files.createSymbolicLink(linkPath, targetPath);
-        
-        Assert.assertFalse(
-            "CheckFileIsNotEmpty returned true.",
-            xUnitValidationService.checkFileIsNotEmpty(linkPath.toFile()));
-        */
-    }
-    
-    @Test
-    @Ignore
-    public void CheckFileIsNotEmpty_GivenLinkToNotEmptyFile_ReturnsTrue() throws Exception
-    {
-        /**
-         * This unit test is disabled as it required Java 7 API.
+        boolean created = testFile.createNewFile();
+        Assert.assertTrue(created);
 
-        FileSystem filesystem = FileSystems.getDefault();
-        
-        File targetFile = CreateNotEmtyFile();
-        Path targetPath = filesystem.getPath(targetFile.getPath());
-        Path linkPath = filesystem.getPath(tempWorkspace.getDir().getPath(), "link.txt");
-        
-        Files.createSymbolicLink(linkPath, targetPath);
-        
-        Assert.assertTrue(
-            "CheckFileIsNotEmpty returned false.",
-            xUnitValidationService.checkFileIsNotEmpty(linkPath.toFile()));
-        */
+        Assert.assertFalse(
+                "CheckFileIsNotEmpty returned true.",
+                xUnitValidationService.checkFileIsNotEmpty(testFile));
     }
-    
+
+    @Test
+    public void CheckFileIsNotEmpty_GivenNotEmptyFile_ReturnsTrue() throws Exception {
+        File testFile = CreateNotEmtyFile();
+
+        Assert.assertTrue(
+                "CheckFileIsNotEmpty returned false.",
+                xUnitValidationService.checkFileIsNotEmpty(testFile));
+    }
+
     /**
      * Helper method that will create a file with some text in it.
-     * 
+     *
      * @return Created file.
-     * 
      * @throws Exception when there is a problem with writing to the file.
      */
-    private File CreateNotEmtyFile() throws Exception
-    {
+    private File CreateNotEmtyFile() throws Exception {
         File testFile = new File(tempWorkspace.getDir(), "notempty.txt");
-        
+
         FileOutputStream stream = new FileOutputStream(testFile);
         stream.write("This is just not empty test file!".getBytes());
         stream.close();
-        
+
         return testFile;
     }
 }
