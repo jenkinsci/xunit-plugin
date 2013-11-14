2ccffb76b86ea71a6c69113c77acf1a7ec8f481f
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-11-14 10:18:18 +0100

    Add GoogleTest support from David Hallas

---
+++ src/main/java/org/jenkinsci/plugins/xunit/types/GoogleTestType.java
@@ -0,0 +1,42 @@
+package org.jenkinsci.plugins.xunit.types;
+
+import com.thalesgroup.dtkit.metrics.hudson.api.descriptor.TestTypeDescriptor;
+import com.thalesgroup.dtkit.metrics.hudson.api.type.TestType;
+import com.thalesgroup.dtkit.metrics.model.InputMetric;
+import com.thalesgroup.dtkit.metrics.model.InputMetricException;
+import com.thalesgroup.dtkit.metrics.model.InputMetricFactory;
+import hudson.Extension;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * @author David Hallas
+ */
+public class GoogleTestType extends TestType {
+
+    @DataBoundConstructor
+    public GoogleTestType(String pattern, boolean ignoreNoResultFiles, boolean failIfNotNew, boolean deleteOutputFiles, boolean stopProcessingIfError) {
+        super(pattern, ignoreNoResultFiles, failIfNotNew, deleteOutputFiles, stopProcessingIfError);
+    }
+
+    @Extension
+    public static class GoogleTestTypeDescriptor extends TestTypeDescriptor<GoogleTestType> {
+
+        public GoogleTestTypeDescriptor() {
+            super(GoogleTestType.class, null);
+        }
+
+        @Override
+        public String getId() {
+            return this.getClass().getName();
+        }
+
+        @Override
+        public InputMetric getInputMetric() {
+            try {
+                return InputMetricFactory.getInstance(GoogleTestInputMetric.class);
+            } catch (InputMetricException e) {
+                throw new RuntimeException("Can't create the inputMetric object for the class " + GoogleTestInputMetric.class);
+            }
+        }
+    }
+}
---
+++ src/test/java/org/jenkinsci/plugins/xunit/types/GoogleTestTypeTest.java
@@ -0,0 +1,18 @@
+package org.jenkinsci.plugins.xunit.types;
+
+import org.junit.Test;
+
+/**
+ * @author David Hallas
+ */
+public class GoogleTestTypeTest extends AbstractTest {
+
+	@Test
+    public void testTestCase1() throws Exception {
+        convertAndValidate(GoogleTestInputMetric.class, "googletest/testcase1/input.xml", "googletest/testcase1/result.xml");
+    }
+	@Test
+    public void testTestCase2() throws Exception {
+        convertAndValidate(GoogleTestInputMetric.class, "googletest/testcase2/input.xml", "googletest/testcase2/result.xml");
+    }
+}
