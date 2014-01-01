7d0496220cfacbd7c60ac735538a22190bedd52d
Peter Jönsson <peter.jonsson@klarna.com>
2014-01-01 17:56:25 +0100

    Add support for Erlang/OTP Junit format
    The Common Test framework in Erlang/OTP can create Jenkins/Hudson
    compatible Junit reports since R15B. In xunit 1.54 these files are
    correctly consumed and rendered by the xUnit plugin.
    
    However in more recent versions the files are not parsed correctly due
    to some extra attributes. This adds support for these attributes. The
    standard Jenkins Junit plugin consumes these files correctly.

---
+++ src/main/java/org/jenkinsci/plugins/xunit/types/model/JUnit10.java
@@ -0,0 +1,34 @@
+package org.jenkinsci.plugins.xunit.types.model;
+
+import com.thalesgroup.dtkit.metrics.model.AbstractOutputMetric;
+
+import java.io.Serializable;
+
+/**
+ * @author Gregory Boissinot
+ */
+public class JUnit10 extends AbstractOutputMetric implements Serializable {
+
+    @Override
+    public String getKey() {
+        return "junit";
+    }
+
+    @Override
+    public String getDescription() {
+        return "JUNIT OUTPUT FORMAT 10.0";
+    }
+
+    @Override
+    public String getVersion() {
+        return "10.0";
+    }
+
+    @Override
+    public String[] getXsdNameList() {
+        return new String[]{"xsd/junit-10.xsd"};
+    }
+}
+
+
+
