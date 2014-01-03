719b511a87bf0ffa31be28c0f9e4d05b9e185e0c
Peter Jönsson <peter.jonsson@klarna.com>
2014-01-03 21:33:10 +0100

    JENKINS-21220 - Make XUnitProcessor serializable
    When processing xUnit files on remote SSH slaves the following error
    was obserserved:
    
     The plugin hasn't been performed correctly: remote file operation failed: /home/jenkins/workspace/xunit at hudson.remoting.Channel@56f87a20:slave
    
    When investigating it was found that a NotSerializableException was
    thrown. Simply make the needed classes serializable.

--- src/main/java/org/jenkinsci/plugins/xunit/ExtraConfiguration.java
+++ src/main/java/org/jenkinsci/plugins/xunit/ExtraConfiguration.java
@@ -1,9 +1,13 @@
 package org.jenkinsci.plugins.xunit;
 
+import java.io.Serializable;
+
 /**
  * @author Gregory Boissinot
  */
-public class ExtraConfiguration {
+public class ExtraConfiguration implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final long testTimeMargin;
 
