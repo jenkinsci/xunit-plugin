fb268c604fb8105e9ae365d2299c40e4450ad27f
etiennebec <etienne.bec@non.schneider-electric.com>
2016-03-10 16:43:11 +0100

    Modify copyright notice to comply with the copyright policy of the company I work for
    Change-Id: Ib6c59ada0587cd0e86a2c34cca275b0823646be6

--- src/test/java/org/jenkinsci/plugins/xunit/types/CUnitTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/CUnitTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016, Etienne Bec
+ * Copyright (c) 2016, Schneider Electric
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
