4bbe1f4322873b7bbea631ec43706bf19d665195
Pekka Enberg <penberg@iki.fi>
2017-03-09 11:16:01 +0200

    Relax BoostTest line validation
    Currently, the BoostTest XML schema definition requires line numbers to
    be positive integers. However, we see cases where Boost Test 1.60
    generates a test log with line number set to zero. While this is
    arguably a Boost Test problem, there's no reason to fail validation and
    potentially the whole test run in Jenkins.
    
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- src/test/java/org/jenkinsci/plugins/xunit/types/BoostTestTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/BoostTestTest.java
@@ -98,4 +98,9 @@ public class BoostTestTest extends AbstractTest {
     public void testTestCase17() throws Exception {
         convertAndValidate(BoostTest.class, "boosttest/testcase17/testlog.xml", "boosttest/testcase17/junit-result.xml");
     }
+
+    @Test
+    public void testTestCase18() throws Exception {
+        convertAndValidate(BoostTest.class, "boosttest/testcase18/testlog.xml", "boosttest/testcase18/junit-result.xml");
+    }
 }
