9c9d03aa8333f81b2b9f428f4b9ade8bb5820313
kpop <kpop@vandewiele.com>
2018-05-03 03:45:49 +0200

    [JENKINS-35660] QTestLib time information is lost
    Use a converter function in XSLT to convert msec attribute of duration
    element for QtTest

--- src/test/java/org/jenkinsci/plugins/xunit/types/QTestlibTypeTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/QTestlibTypeTest.java
@@ -1,6 +1,30 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2014, Gregory Boissinot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jenkinsci.plugins.xunit.types;
 
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 
 /**
  * @author Gregory Boissinot
@@ -14,6 +38,12 @@ public class QTestlibTypeTest extends AbstractTest {
 
     @Test
     public void testTestCase2() throws Exception {
+        convertAndValidate(QTestLibInputMetric.class, "qtestlib/testcase2/input.xml", "qtestlib/testcase2/result.xml");
+    }
+
+    @Issue("JENKINS-35660")
+    @Test
+    public void verify_that_duration_converted_correctly() throws Exception {
         convertAndValidate(QTestLibInputMetric.class, "qtestlib/testcase2/input.xml", "qtestlib/testcase2/result.xml");
     }
 
