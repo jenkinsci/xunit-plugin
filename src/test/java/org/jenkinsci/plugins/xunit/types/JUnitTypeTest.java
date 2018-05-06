3f23643dc1646f1ff2f4b6b3b8dc8251b7d62d17
Nikolas Falco <nfalco79@hotmail.com>
2018-05-06 20:34:37 +0200

    Improve JUnit XSD to be more surefire report compliant.

--- src/test/java/org/jenkinsci/plugins/xunit/types/JUnitTypeTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/JUnitTypeTest.java
@@ -1,34 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2014, Gregory Boissinot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jenkinsci.plugins.xunit.types;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * @author Gregory Boissinot
  */
+@RunWith(Parameterized.class)
 public class JUnitTypeTest extends AbstractTest {
 
-    @Test
-    public void testTestCase1() throws Exception {
-        convertAndValidate(JUnitInputMetric.class, "junit/testcase1/input.xml", "junit/testcase1/input.xml");
+    @Parameters(name = "{0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { { "test case 1", 1 }, //
+                                              { "test case 2", 2 }, //
+                                              { "test case 3", 3 }, //
+                                              { "test case 4", 4 }, //
+                                              { "test case 5", 5 }
+        });
     }
 
-    @Test
-    public void testTestCase2() throws Exception {
-        convertAndValidate(JUnitInputMetric.class, "junit/testcase2/input.xml", "junit/testcase2/input.xml");
+    public JUnitTypeTest(String testName, int testNumber) {
+        super(JUnitInputMetric.class, resolveInput("junit", testNumber), resolveInput("junit", testNumber));
     }
 
+    @Override
     @Test
-    public void testTestCase3() throws Exception {
-        convertAndValidate(JUnitInputMetric.class, "junit/testcase3/input.xml", "junit/testcase3/input.xml");
+    public void verifyXSLT() throws Exception {
+        super.verifyXSLT();
     }
 
-    @Test
-    public void testTestCase4() throws Exception {
-        convertAndValidate(JUnitInputMetric.class, "junit/testcase4/input.xml", "junit/testcase4/input.xml");
-    }
-
-    @Test
-    public void testTestCase5() throws Exception {
-    	convertAndValidate(JUnitInputMetric.class, "junit/testcase5/input.xml", "junit/testcase5/input.xml");
-    }
 }
