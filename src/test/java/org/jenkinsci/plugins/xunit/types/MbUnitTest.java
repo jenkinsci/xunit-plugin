c4cdfe89496f12b170900045ffe60ee216447fcf
Nikolas Falco <nfalco79@hotmail.com>
2018-05-11 02:24:53 +0200

    Improve MbUnit XSD to produce report more surefire compliant.

---
+++ src/test/java/org/jenkinsci/plugins/xunit/types/MbUnitTest.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014, Gregory Boissinot, Falco Nikolas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.xunit.types;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MbUnitTest extends AbstractTest {
+
+    @Parameters(name = "testcase{1}: {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { { "testTestCase1", 1 } //
+        });
+    }
+
+    public MbUnitTest(String testName, int testNumber) {
+        super(MbUnitInputMetric.class, resolveInput("mbunit", testNumber), resolveOutput("mbunit", testNumber));
+    }
+
+    @Override
+    @Test
+    public void verifyXSLT() throws Exception {
+        super.verifyXSLT();
+    }
+
+}
