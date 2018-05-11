9aaab6e40e4c66ea6c1268beffcfa826ba4707d2
Nikolas Falco <nfalco79@hotmail.com>
2018-05-11 02:23:55 +0200

    [JENKINS-10911] xUnit not detecting Ignored MSTest and NUnit Tests
    Support skipped testcase for MSTest using Inconclusive value for outcome attribute of the UnitTestResult XML element.

--- src/test/java/org/jenkinsci/plugins/xunit/types/MSTestTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/MSTestTest.java
@@ -1,56 +1,57 @@
-/*******************************************************************************
- * Copyright (c) 2010 Thales Corporate Services SAS                             *
- * Author : Gregory Boissinot                                                   *
- *                                                                              *
- * Permission is hereby granted, free of charge, to any person obtaining a copy *
- * of this software and associated documentation files (the "Software"), to deal*
- * in the Software without restriction, including without limitation the rights *
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
- * copies of the Software, and to permit persons to whom the Software is        *
- * furnished to do so, subject to the following conditions:                     *
- *                                                                              *
- * The above copyright notice and this permission notice shall be included in   *
- * all copies or substantial portions of the Software.                          *
- *                                                                              *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
- * THE SOFTWARE.                                                                *
- *******************************************************************************/
-
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014, Gregory Boissinot, Falco Nikolas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jenkinsci.plugins.xunit.types;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized.class)
 public class MSTestTest extends AbstractTest {
 
-    @Test
-    public void testTransformationCase1() throws Exception {
-        convertAndValidate(MSTest.class, "mstest/mstest_2_tests_1_class.trx", "mstest/junit_mstest_2_tests_1_class.xml");
+    @Parameters(name = "testcase{1}: {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { { "mstest_2_tests_1_class", 1 }, //
+                                              { "mstest_2_tests_from_different_assemblies", 2 }, //
+                                              { "mstest_4_tests_2_classes", 3 }, //
+                                              { "mstest_vs_2010", 4 }, //
+                                              { "mstest_more_than_one_minute_test", 5 }, //
+                                              { "JENKINS-10911", 6 } //
+        });
     }
 
-    @Test
-    public void testTransformationCase2() throws Exception {
-        convertAndValidate(MSTest.class, "mstest/mstest_2_tests_from_different_assemblies.trx", "mstest/junit_mstest_2_tests_from_different_assemblies.xml");
+    public MSTestTest(String testName, int testNumber) {
+        super(MSTest.class, resolveInput("mstest", testNumber), resolveOutput("mstest", testNumber));
     }
 
+    @Override
     @Test
-    public void testTransformationCase3() throws Exception {
-        convertAndValidate(MSTest.class, "mstest/mstest_4_tests_2_classes.trx", "mstest/junit_mstest_4_tests_2_classes.xml");
-    }
-
-    @Test
-    public void testTransformationCase4() throws Exception {
-        convertAndValidate(MSTest.class, "mstest/mstest_vs_2010.trx", "mstest/junit_mstest_vs_2010.xml");
-    }
-
-    @Test
-    public void testTransformationCase5() throws Exception {
-        convertAndValidate(MSTest.class, "mstest/mstest_more_than_one_minute_test.trx", "mstest/junit_mstest_more_than-one_minute_test.xml");
+    public void verifyXSLT() throws Exception {
+        super.verifyXSLT();
     }
 }
-
