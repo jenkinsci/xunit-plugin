f43a8e5411f627970901c30a58abe35293651c55
Nikolas Falco <nikolas.falco@finantix.com>
2018-08-16 17:30:25 +0200

    [JENKINS-53034] Failure when validate NUnit 2.x reports generated by NUnit 3.x console
    Relax original XSD to allow more assembly test-suite as child of root element.

--- src/test/java/org/jenkinsci/plugins/xunit/types/NUnitTest.java
+++ src/test/java/org/jenkinsci/plugins/xunit/types/NUnitTest.java
@@ -50,7 +50,8 @@ public class NUnitTest extends AbstractTest {
                                               { "JENKINS-51556 cause JEP-200 issue", 12 }, //
                                               { "JENKINS-51561 NUnit 3.x produce report in NUnit 2.x format", 13 }, //
                                               { "JENKINS-51767", 14 }, //
-                                              { "JENKINS-52107", 15 } //
+                                              { "JENKINS-52107", 15 }, //
+                                              { "JENKINS-53034", 16 } //
         });
     }
 
